591c59ec0e91a2e41a5c4e6d13a5e67b2a9ce02f
MartynyukAndriy <Andriy.martynyuk.if@gmail.com>
2023-09-28 00:56:54 +0300

    changed main

--- main.py
+++ main.py
@@ -3,26 +3,34 @@ import uvicorn
 from fastapi import FastAPI
 
 # from fastapi_limiter import FastAPILimiter
-# from src.conf.config import settings
+from src.conf.config import settings
 from sqladmin import Admin
 
-from src.database.db import sessionmanager
+from src.database.db import create_async_engine
+
 from src.routes import auth, users, countries, cities, currency
 
 
+engine = create_async_engine(settings.sqlalchemy_database_url)
+
+
 # Створюємо екземпляр FastApi, встановлюємо назву додатка у swagger та відсортуємо роути по методах:
-from src.services.admin_panel.admin_panel import UserAdmin, CityAdmin, CountryAdmin, SubscribePlanAdmin
+from src.services.admin_panel.admin_panel import UserAdmin, MasterInfoAdmin, CityAdmin, CountryAdmin, \
+    SubscribePlanAdmin, UserResponseAdmin, ServiceAdmin, ServiceCategoryAdmin
 
 app = FastAPI(swagger_ui_parameters={"operationsSorter": "method"}, title='Platforma17 app')
 
-# підключаємо адмінку
+# підключаємо адмін-панель
 # http://localhost:8001/admin/
-admin = Admin(app, sessionmanager)
+admin = Admin(app, engine)
 admin.add_view(UserAdmin)
+admin.add_view(MasterInfoAdmin)
 admin.add_view(CountryAdmin)
 admin.add_view(CityAdmin)
 admin.add_view(SubscribePlanAdmin)
-
+admin.add_view(UserResponseAdmin)
+admin.add_view(ServiceCategoryAdmin)
+admin.add_view(ServiceAdmin)
 
 
 @app.get("/")
@@ -32,7 +40,8 @@ def root():
 
 # @app.on_event("startup")
 # async def startup():
-#     r = await redis.Redis(host=settings.redis_host, port=settings.redis_port, password=settings.redis_password, encoding="utf-8", db=0)
+#     r = await redis.Redis(host=settings.redis_host, port=settings.redis_port, password=settings.redis_password,
+#     encoding="utf-8", db=0)
 #     await FastAPILimiter.init(r)
 
 app.include_router(auth.router, prefix='/api')
@@ -40,6 +49,7 @@ app.include_router(users.router, prefix='/api')
 app.include_router(countries.router, prefix='/api')
 app.include_router(cities.router, prefix='/api')
 app.include_router(currency.router, prefix='/api')
+
 
 if __name__ == '__main__':
     uvicorn.run('main:app', reload=True)
